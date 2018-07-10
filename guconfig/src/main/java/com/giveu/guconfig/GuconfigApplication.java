002a41b77b3a2d81b89c28bc54cd2c42bc0e7d1a
yinhai <1239128278@qq.com>
2018-07-10 21:00:08 +0800

    配置中心
    配置中心

--- guconfig/src/main/java/com/giveu/guconfig/GuconfigApplication.java
+++ guconfig/src/main/java/com/giveu/guconfig/GuconfigApplication.java
@@ -2,8 +2,11 @@ package com.giveu.guconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GuconfigApplication {
 
 	public static void main(String[] args) {
