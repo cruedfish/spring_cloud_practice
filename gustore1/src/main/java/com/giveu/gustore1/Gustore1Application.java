686319e74e5a7591684108b695acbba34fdaf310
yinhai <1239128278@qq.com>
2018-07-11 10:56:14 +0800

    gustore1提交

---
+++ gustore1/src/main/java/com/giveu/gustore1/Gustore1Application.java
@@ -0,0 +1,12 @@
+package com.giveu.gustore1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gustore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gustore1Application.class, args);
+	}
+}
---
+++ gustore1/src/test/java/com/giveu/gustore1/Gustore1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.giveu.gustore1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gustore1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
